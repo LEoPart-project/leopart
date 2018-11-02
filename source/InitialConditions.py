c16292f58aeaf00cef0658c720894a94c65eee3d
Chris Richardson <chris@bpi.cam.ac.uk>
2018-11-02 10:33:41 +0000

    Clear all flake8 warnings

--- source/InitialConditions.py
+++ source/InitialConditions.py
@@ -8,39 +8,47 @@ import numpy as np
 
 __all__ = ['GaussianPulse', 'SineHump']
 
+
 class GaussianPulse(UserExpression):
     """
     Overloaded expression for initializing a Gaussian pulse with variance sigma
     and centered at center.
     """
-    def __init__(self,center, sigma, U, time=0, height = 1.0, **kwargs):
+
+    def __init__(self, center, sigma, U, time=0, height=1.0, **kwargs):
         self.center = center
-        self.sigma  = sigma
+        self.sigma = sigma
         self.height = height
-        self.U      = U
-        self.t      = time
+        self.U = U
+        self.t = time
         super().__init__(self, **kwargs)
 
     def eval(self, value, x):
         xc = self.center[0]
         yc = self.center[1]
-        U,t,sigma  = self.U, self.t, self.sigma
-        #value[0] = self.height * np.exp(-(pow(x[0] - xc - U[0]* x[1] * self.t,2)+pow(x[1]-yc - U[1]* x[0] * self.t,2))/(2*pow(self.sigma,2)))
+        U, t, sigma = self.U, self.t, self.sigma
+        # value[0] = self.height * np.exp(-(pow(x[0] - xc - U[0]* x[1] * self.t,2)
+        #                   +pow(x[1]-yc - U[1]* x[0] * self.t,2))/(2*pow(self.sigma,2)))
 
-        value[0] = self.height * np.exp( -(pow(x[0]*np.cos(U[0]*t) + x[1]*np.sin(U[1]*t) - xc,2)+pow(-x[0]*np.sin(U[0]*t) + x[1]*np.cos(U[1]*t)-yc,2))/(2*pow(sigma,2) ))
+        value[0] = self.height * np.exp(-(pow(x[0]*np.cos(U[0]*t)
+                                              + x[1]*np.sin(U[1]*t) - xc, 2)
+                                          + pow(-x[0]*np.sin(U[0]*t)
+                                                + x[1]*np.cos(U[1]*t)-yc, 2))/(2*pow(sigma, 2)))
 
     def value_shape(self):
         return ()
 
+
 class SineHump(UserExpression):
     def __init__(self, center, U, time, **kwargs):
         self.center = center
-        self.U      = U
-        self.t      = time
+        self.U = U
+        self.t = time
         super().__init__(self, **kwargs)
 
     def eval(self, value, x):
         xc = self.center[0]
         yc = self.center[1]
-        U, t   = self.U, self.t
-        value[0] = np.sin(2*pi*(x[0] - xc - U[0]*t)) * np.sin(2*pi*(x[1]-yc - U[1]*t))
+        U, t = self.U, self.t
+        value[0] = np.sin(2*pi*(x[0] - xc - U[0]*t)) * \
+            np.sin(2*pi*(x[1]-yc - U[1]*t))
--- source/__init__.py
+++ source/__init__.py
@@ -1,5 +1,9 @@
-from .ParticleFun import *
+# flake8: noqa
+
+from .ParticleFun import (particles, advect_particles, advect_rk2, advect_rk3, l2projection,
+                          StokesStaticCondensation, PDEStaticCondensation, AddDelete)
 from .FormsPDEMap import FormsPDEMap
 from .FormsStokes import FormsStokes
-from .InitialConditions import *
-from .ParticleGenerator import *
+from .InitialConditions import GaussianPulse, SineHump
+from .ParticleGenerator import (RandomRectangle, RandomCircle, RandomBoxRandomSphere,
+                                RegularRectangle, RegularBox)
