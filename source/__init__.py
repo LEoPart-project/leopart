32e677fe481d8c76a3c09713a8a9616fb788e5f6
jmmaljaars <jm2282@cockatoo.bpi.cam.ac.uk>
2018-11-02 11:51:52 +0000

    Fix in import

--- source/__init__.py
+++ source/__init__.py
@@ -5,4 +5,5 @@ from .ParticleFun import (particles, advect_particles, advect_rk2, advect_rk3, l
 from .FormsPDEMap import FormsPDEMap
 from .FormsStokes import FormsStokes
 from .InitialConditions import GaussianPulse, SineHump
-from .ParticleGenerator import (RandomRectangle, RandomCircle, RegularRectangle, RegularBox)
+from .ParticleGenerator import (RandomRectangle, RandomCircle, RegularRectangle, RegularBox,
+                                RandomBox, RegularBox)
