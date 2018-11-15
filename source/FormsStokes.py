4b6efe988ed930af5ab31ab2c2be8d8d480e04e5
Jakob Maljaars <jakobmaljaars@gmail.com>
2018-11-15 17:30:41 +0100

    Towards two fluids: forms

--- source/FormsStokes.py
+++ source/FormsStokes.py
@@ -6,7 +6,7 @@
 
 from dolfin import (Form, FacetNormal, CellDiameter, ds, dx, dS,
                     div, dot, sym, grad, inner, TestFunctions,
-                    TrialFunctions, Constant, Identity)
+                    TrialFunctions, Constant, Identity, outer)
 import numpy as np
 
 
@@ -46,16 +46,16 @@ class FormsStokes:
         self.he = CellDiameter(mesh)
         self.ds = ds
         self.gdim = mesh.geometry().dim()
+        # TODO: class can be much condensed
+        # Note ds(98) will be marked as free-slip boundary
 
     def forms_steady(self, nu, f):
         '''
         Steady Stokes
         '''
 
-        w, q = TestFunctions(self.mixedL)
-        u, p = TrialFunctions(self.mixedL)
-        wbar, qbar = TestFunctions(self.mixedG)
-        ubar, pbar = TrialFunctions(self.mixedG)
+        (w, q, wbar, qbar) = self.__test_functions()
+        (u, p, ubar, pbar) = self.__trial_functions()
 
         # Infer geometric dimension
         zero_vec = np.zeros(self.gdim)
@@ -111,10 +111,8 @@ class FormsStokes:
         Forms for Backward-Euler time integration
         '''
 
-        w, q = TestFunctions(self.mixedL)
-        u, p = TrialFunctions(self.mixedL)
-        wbar, qbar = TestFunctions(self.mixedG)
-        ubar, pbar = TrialFunctions(self.mixedG)
+        (w, q, wbar, qbar) = self.__test_functions()
+        (u, p, ubar, pbar) = self.__trial_functions()
 
         # Infer geometric dimension
         zero_vec = np.zeros(self.gdim)
@@ -169,8 +167,63 @@ class FormsStokes:
         S_S = facet_integral(dot(Constant(zero_vec), wbar))
         return self.__get_form_dict(A_S, G_S, G_ST, B_S, Q_S, S_S)
 
+    def forms_multiphase(self, rho, ustar, dt, mu, f):
+
+        (w, q, wbar, qbar) = self.__test_functions()
+        (u, p, ubar, pbar) = self.__trial_functions()
+
+        # Infer geometric dimension
+        zero_vec = np.zeros(self.gdim)
+
+        ds = self.ds
+        n = self.n
+        he = self.he
+        alpha = self.alpha
+        beta_stab = self.beta_stab
+        facet_integral = self.facet_integral
+
+        pI = p*Identity(self.mixedL.sub(1).ufl_cell().topological_dimension())
+        pbI = pbar * \
+            Identity(self.mixedL.sub(1).ufl_cell().topological_dimension())
+
+        # Upper left block
+        # Contribution comes from local momentum balance
+        AB = rho * dot(u, w)/dt * dx \
+            + inner(2*mu*sym(grad(u)), grad(w))*dx \
+            + facet_integral(dot(-2*mu*sym(grad(u))*n + (2*mu*alpha/he)*u, w)) \
+            + facet_integral(dot(-2*mu*u, sym(grad(w))*n)) \
+            - inner(pI, grad(w))*dx
+        BtF = -dot(q, div(u))*dx - facet_integral(beta_stab*he/(mu + 1)*dot(p, q))
+        A_S = AB + BtF
+
+        # Upper right block G
+        CD = facet_integral(-alpha/he*2*mu*inner(ubar, w)) \
+            + facet_integral(2*mu*inner(ubar, sym(grad(w))*n)) \
+            + facet_integral(dot(pbI*n, w))
+        H = facet_integral(beta_stab*he/(mu+1)*dot(pbar, q))
+        G_S = CD + H
+
+        # Transpose block
+        CDT = facet_integral(-alpha/he*2*mu*inner(wbar, u)) \
+            + facet_integral(2*mu*inner(wbar, sym(grad(u))*n)) \
+            + facet_integral(qbar * dot(u, n))
+        HT = facet_integral(beta_stab*he/(mu+1)*dot(p, qbar))
+        G_ST = CDT + HT
+
+        # Lower right block B
+        KL = facet_integral(alpha/he * 2*mu*dot(ubar, wbar)) \
+            - facet_integral(dot(pbar*n, wbar)) \
+            + Constant(1E12)/he * inner(outer(ubar, wbar), outer(n, n)) * ds(98)
+        LtP = - facet_integral(dot(ubar, n)*qbar) \
+              - facet_integral(beta_stab*he/(mu + 1) * pbar * qbar)
+        B_S = KL + LtP
+
+        # Righthandside
+        Q_S = rho * dot(f, w)*dx + rho * dot(ustar, w)/dt * dx
+        S_S = facet_integral(dot(Constant(zero_vec), wbar))
+        return self.__get_form_dict(A_S, G_S, G_ST, B_S, Q_S, S_S)
+
     def facet_integral(self, integrand):
-        # - integrand *self.ds(99)
         return integrand('-')*dS + integrand('+')*dS + integrand*ds
 
     def __get_form_dict(self, A_S, G_S, G_ST, B_S, Q_S, S_S):
@@ -183,3 +236,13 @@ class FormsStokes:
         S_S = Form(S_S)
         return {'A_S': A_S, 'G_S': G_S, 'G_ST': G_ST,
                 'B_S': B_S, 'Q_S': Q_S, 'S_S': S_S}
+
+    def __test_functions(self):
+        w, q = TestFunctions(self.mixedL)
+        wbar, qbar = TrialFunctions(self.mixedG)
+        return (w, q, wbar, qbar)
+
+    def __trial_functions(self):
+        u, p = TrialFunctions(self.mixedL)
+        ubar, pbar = TrialFunctions(self.mixedG)
+        return (u, p, ubar, pbar)
